4b3c156e218550d48b9d0e8cc3e0f68b286ae8f2
Ethan Frey <ethanfrey@users.noreply.github.com>
2016-10-05 00:38:09 +0200

    Add skeleton for the tmsp application

---
+++ application.go
@@ -0,0 +1,44 @@
+package bloggermint
+
+import (
+	merkle "github.com/tendermint/go-merkle"
+	tmsp "github.com/tendermint/tmsp/types"
+)
+
+type Application struct {
+	tree *merkle.IAVLTree
+}
+
+// Info is a placeholder
+func (app *Application) Info() string {
+	return "Hi from bloggermint"
+}
+
+// SetOption is ignored for now
+func (app *Application) SetOption(key, value string) string {
+	return "ignored"
+}
+
+// AppendTX actually does something :)
+func (app *Application) AppendTx(tx []byte) tmsp.Result {
+	// TODO
+	return tmsp.Result{}
+}
+
+// CheckTX validates a tx for the mempool
+func (app *Application) CheckTx(tx []byte) tmsp.Result {
+	// TODO
+	return tmsp.Result{}
+}
+
+// Query checks the current state
+func (app *Application) Query(query []byte) tmsp.Result {
+	// TODO
+	return tmsp.Result{}
+}
+
+// Commit returns the application Merkle root hash
+func (app *Application) Commit() tmsp.Result {
+	// TODO
+	return tmsp.Result{}
+}
